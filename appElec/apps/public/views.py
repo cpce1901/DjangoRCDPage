e12829b4ff4babed9ccae02482631348082fe7fa
cpce1901 <ircd.claudio@gmail.com>
2023-11-11 20:01:00 -0300

    urlImage

--- appElec/apps/public/views.py
+++ appElec/apps/public/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from apps.private.models import Logo
 
 # Create your views here.
 class Home(TemplateView):
     template_name = 'public/home.html'
+
+    
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        logo = Logo.objects.filter(name = "logo")
+        context["logo"] = logo
+        return context
+    
+
+    
 
 # Create your views here.
 class About(TemplateView):
