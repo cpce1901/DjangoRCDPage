c53fb85c4cc643fbc87d945d9d5101c3245bf738
cpce1901 <ircd.claudio@gmail.com>
2023-11-11 13:28:39 -0300

    cambiosEnMediaUpload

--- appElec/apps/private/models.py
+++ appElec/apps/private/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Logo(models.Model):
     name = models.CharField('Logo',max_length=128, unique=True)
-    image = models.ImageField()
+    image = models.ImageField(upload_to= 'media/private')
 
     def __str__(self):
         return f"{self.name}"
