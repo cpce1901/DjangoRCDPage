9662e0794fd82cf1ce7157a704f77a94f3105a68
cpce1901 <ircd.claudio@gmail.com>
2023-11-19 22:52:23 -0300

    updatetemplate

--- appElec/apps/private/admin.py
+++ appElec/apps/private/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Logo, Material
+from .models import Logo
 
 # Register your models here.
 admin.site.register(Logo)
-admin.site.register(Material)
+
--- appElec/apps/private/models.py
+++ appElec/apps/private/models.py
@@ -4,11 +4,10 @@ import os
 
 # Create your models here.
 class Logo(models.Model):
-    
     def change_name(instance, filename):
-        ext = filename.split('.')[-1]
+        ext = filename.split(".")[-1]
         filename = f"logo.{ext}"
-        return os.path.join('media/private', filename)
+        return os.path.join("media/private", filename)
 
     name = models.CharField("Logo", max_length=128, unique=True)
     image = models.ImageField(upload_to=change_name)
@@ -17,11 +16,3 @@ class Logo(models.Model):
         return f"{self.name}"
 
 
-class Material(models.Model):
-    code = models.PositiveIntegerField("Codigo", unique=True)
-    description = models.CharField("Descripcion", max_length=1024)
-    unit = models.CharField("Unidad", max_length=8)
-    price = models.PositiveIntegerField("Precio")
-
-    def __str__(self):
-        return f"{self.code} - {self.description} - {self.unit} - {self.price}"
