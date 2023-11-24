ea486a3bc307f07c10393986a2eb7e3f6d4dd25e
cpce1901 <ircd.claudio@gmail.com>
2023-11-24 00:01:55 -0300

    adminMobileToken

--- appElec/apps/private/admin.py
+++ appElec/apps/private/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Logo
+from .models import Logo, MobileToken
 
 # Register your models here.
 admin.site.register(Logo)
+admin.site.register(MobileToken)
 
